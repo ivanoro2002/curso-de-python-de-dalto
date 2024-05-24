8fc5884fcbf0f3138d5a361ba37c8f0b79b65e6c
ivan valicenti <ivannazaorosco777@gmail.com>
2024-05-23 21:13:05 -0300

    subida 1

---
+++ funciones/funciones_datos_extra.py
@@ -0,0 +1,5 @@
+def frase (nombre,apellido,adjetivo,):
+    return f'hola {nombre} {apellido}, sos muy {adjetivo}'
+
+frase_resultante = frase(adjetivo="capo",nombre="lucas",apellido="dalto")
+print(frase_resultante)
--- funciones/parametros_ars.py
+++ funciones/parametros_ars.py
@@ -1,5 +1,6 @@
 #utilizando el parametro args 
-def suma (*numeros):
-    return sum (numeros)
-resultado= suma (4,5,6,7,8)
+def suma (nombre,*numeros):
+ return f"sum{nombre}la suma de tus numeros es {sum(numeros)}"
+
+resultado = suma("lucas",4,5,6,7,8)
 print (resultado)
