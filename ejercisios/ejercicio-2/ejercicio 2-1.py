9e0726d9829406b197ea9427e78550b93e1d5129
ivan <valicentiivan@gmail.com>
2024-06-07 21:39:18 -0300

    subida 4

--- ejercisios/ejercicio-2/ejercicio 2-1.py
+++ ejercisios/ejercicio-2/ejercicio 2-1.py
@@ -1,16 +1,30 @@
 # falto el profe y los pibes van a armar la clase
 #pedir el nombre y la edad de los compañeros que vinieron hoy a clase
-def obtener_compañeros (cantidad_de_compañeros):
-    compañeros =[]
-    for i in range (cantidad_de_compañeros):
-         nombre= input("ingrese el nombre del compañero:   ") 
-         edad = int(input("ingrese la edad del compañero:   "))
-         compañero = (nombre,edad)
-         compañeros.append (compañero)
-    compañeros.sort (key=lambda x:x [1])
-    asistente = compañeros [0][0]
-    profesor = compañeros [-1] [0]
-    return asistente,profesor
-asistente,profesor= obtener_compañeros[0]
+def obtener_compañeros(cantidad_de_compañeros):
+   
+#creando la lista de compañeros
+    compañeros = []
+    
+    #ejecutando un for para pedir informacion de cada compañero
+    for i in range(cantidad_de_compañeros):
+        
+        nombre = input("Ingrese el nombre del compañero: ") 
+        edad = int(input("Ingrese la edad del compañero: "))
+        compañero = (nombre, edad)
+        
+        #agregando la informacion a la lista
+        compañeros.append(compañero)
+        
+ #ordenandolos de menor a mayor segun su edad       
+    compañeros.sort(key=lambda x: x[1])
+    
+ # compañeros [x] devuelve una tupla con (nombre,edad) y despues accedemos al nombre  
+ # para definir al asistente y el profesor
+    asistente = compañeros[0][0]
+    profesor = compañeros[-1][0]
+    return asistente, profesor
+
+# Llama a la función y pasa la cantidad de compañeros como argumento
+asistente, profesor = obtener_compañeros(3)
 
 print(f'el profesor es: {profesor} y su asistente es: {asistente}')
---
+++ ejercisios/ejercicio-2/ejercicio 2-2.py
@@ -0,0 +1,6 @@
+#creando una funcion que nos devuelva los numeros primos
+# entre el 0 y el argumento que pasamos
+
+def es_primo (num):
+    for i in range (num):
+        if num%i==0
